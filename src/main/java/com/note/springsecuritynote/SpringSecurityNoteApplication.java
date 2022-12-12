a34d901eb552d183505bf40bb49fbcfc38b5a7d6
yeongjin han <hanyj0104@naver.com>
2022-12-12 17:05:09 +0900

    플젝시작

---
+++ src/main/java/com/note/springsecuritynote/SpringSecurityNoteApplication.java
@@ -0,0 +1,13 @@
+package com.note.springsecuritynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityNoteApplication.class, args);
+	}
+
+}
